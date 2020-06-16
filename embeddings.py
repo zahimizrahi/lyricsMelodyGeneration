34b44d1c0077b38e903f704f603093b50b078a2f
aviad <aviadazriel2@gmail.com>
2020-06-16 13:36:00 +0300

    DOC2VEC_MODELS_PATHS

--- embeddings.py
+++ embeddings.py
@@ -15,13 +15,13 @@ def extract_embedding_weights():
     # prepare embedding matrix
     word_index = tokenizer.word_index
     num_words = len(word_index) + 1
-    print(gc.collect())
+    gc.collect()
     print("stage1 -Done")
     pretrained_embeddings = load_pretrained_embedding()
-    print(gc.collect())
+    gc.collect()
     print("stage2 -Done")
     embedding_matrix, not_found = prepare_embedding_matrix(num_words, EMBEDDING_DIM, word_index, pretrained_embeddings)
-    print(gc.collect())
+    gc.collect()
     print("stage3 -Done")
     return embedding_matrix
 
@@ -39,10 +39,32 @@ def prepare_embedding_matrix(num_of_words, embedding_dim, word_index, pretrained
     return embedding_matrix, not_found
 
 def load_pretrained_embedding():
-    embeddings_index = {}
-    with open(os.path.join(GLOVE_DIR, 'glove.6B.300d.txt'), 'rb') as f:
-        for line in f:
-            word, coefs = line.split(maxsplit=1)
-            coefs = np.fromstring(coefs, 'f', sep=' ')
-            embeddings_index[word] = coefs
+    local_pickle_file = os.path.join(DOC2VEC_MODELS_PATHS, 'glove_embeddings.pickle')
+    if os.path.exists(local_pickle_file):
+        print("Loading glove_embeddings")
+        with open(local_pickle_file, 'rb') as handle:
+            embeddings_index = pickle.load(handle)
+    else:
+        embeddings_index = {}
+        with open(os.path.join(GLOVE_DIR, 'glove.6B.300d.txt'), 'rb') as f:
+            for line in f:
+                word, coefs = line.split(maxsplit=1)
+                coefs = np.fromstring(coefs, 'f', sep=' ')
+                embeddings_index[word] = coefs
     return embeddings_index
+
+#### our model
+def load_embedding(self, filename='glove_pretrained/glove.6B.300d.txt'):
+    """
+    loads the embedding as dictionary of words vectors. Keys are distinct words
+    and values are the word vectors.
+    :param filename - a .txt file of the pretrained word2vec embedding vectors
+    """
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    split_lines = map(lambda line: line.split(), lines)
+
+    embedding = {parts[0]: np.asarray(parts[1:], dtype='float32') for parts in split_lines}
+
+    return embedding
