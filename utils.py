88df27cf5785e5bec0818e4e10f9b1a75187738c
aviad <aviadazriel2@gmail.com>
2020-06-23 19:51:33 +0300

    cosine

--- utils.py
+++ utils.py
@@ -61,11 +61,37 @@ def idx2word(index, tokenizer):
         if idx == index:
             return word
 
-def cosine_distance_wordembedding_method(s1, s2):
-    word_model =load_pretrained_embedding()
-    vector_1 = np.mean([word_model[word] for word in s1.split()],axis=0)
-    vector_2 = np.mean([word_model[word] for word in s2.split()],axis=0)
-    cosine = scipy.spatial.distance.cosine(vector_1, vector_2)
-    cosinn_score = round((1-cosine)*100,2)
-    # print('Word Embedding method with a cosine distance asses that our two sentences are similar to',cosinn_score,'%')
-    return cosinn_score
+def cosine_distance_wordembedding_method(s1, s2, word_model=None):
+    cosine_scores = []
+    if word_model is None:
+        word_model =load_pretrained_embedding()
+
+    s1 = s1.split()
+    s2 = s2.split()
+    assert len(s1) == len(s2)
+    for index, word in enumerate(s1):
+      unk_tag = 'unk_embedding'
+      try:
+        s1_emb = word_model[word]
+      except:
+        s1_emb = word_model[unk_tag]
+      try:
+        s2_emb = word_model[s2[index]]
+      except:
+        s2_emb = word_model[unk_tag]
+      cosine = scipy.spatial.distance.cosine(s2_emb, s1_emb)
+      cosinn_score = round((1-cosine)*100,2)
+      cosine_scores.append(cosinn_score)
+    return np.array(cosine_scores).mean()
+
+def calculate_similarity(df, lyrics_generate_dict):
+  word_model =load_pretrained_embedding()
+  cosine_similarity_list = []
+  for key in lyrics_generate_dict:
+    num_word = len(lyrics_generate_dict[key].split())
+    original_lyrics = ' '.join(list(df[df['SongName'] == key ]['X'])[0][:num_word])
+    cosine_similarity_list.append(cosine_distance_wordembedding_method2(original_lyrics , lyrics_generate_dict[key], word_model= word_model))
+
+  cosinn_score = np.array(cosine_similarity_list).mean()
+  print('Word Embedding method with a cosine distance asses that our two sentences are similar to',cosinn_score,'%')
+  return cosinn_score
